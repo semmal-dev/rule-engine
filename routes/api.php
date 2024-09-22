2cdbad415c309df79240683b308844dd1f419481
omnify-semmal <semmal@getomnify.com>
2024-09-22 23:00:35 +0530

    Added queues and jobs for dependencies scan

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,13 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\VulnerabilityScanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
-// Route::post('/register', function (Request $request) {
-//     return "hai";
-// });
 
 Route::post('login', [UserController::class, 'login'])->name('login');
 Route::post('register', [UserController::class, 'register'])->name('register');
 Route::group(['middleware' => 'auth:api'], function(){
-    Route::post('details', 'API\UserController@details');
+    Route::post('upload', [VulnerabilityScanController::class,'uploadDependencies'])->name('uploadDependencies');
 });
